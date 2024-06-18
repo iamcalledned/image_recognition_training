2ff2bf311fa676ce133f7e78c71c47ed568fc03e
Ned <iamcalledned@gmail.com>
2024-06-18 17:39:17 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -32,3 +32,20 @@ def predict_images(net, testloader, classes):
 
     print('Predicted: ', ' '.join(f'{classes[predicted[j]]}' for j in range(4)))
 
+def visualize_misclassifications(net, testloader, classes):
+    misclassified = []
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            for i in range(len(labels)):
+                if predicted[i] != labels[i]:
+                    misclassified.append((images[i], predicted[i], labels[i]))
+
+    print(f'Total misclassified images: {len(misclassified)}')
+
+    # Visualize some misclassified images
+    for img, pred, true in misclassified[:5]:  # Show first 5 misclassified images
+        imshow(img)
+        print(f'Predicted: {classes[pred]}, Actual: {classes[true]}')
